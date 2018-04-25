2191a13db55821b271786d1cd31d065d5b5a680c
Dave Hudson <dave@applificationhq.com>
2018-04-25 14:49:10 +0100

    README and updated example

--- App.js
+++ App.js
@@ -1,23 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { BodyText } from 'react-native-minimalist';
+import { StyleSheet, View } from 'react-native';
+import {
+  Header,
+  Photo,
+  Text,
+  Profile,
+  Divider,
+  PhotoInfo
+} from 'react-native-minimalist';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 40,
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  mobile: {
+    width: 375
+  }
+});
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!!!</Text>
-        <BodyText>React Native Minimalist...</BodyText>
+        <View style={styles.mobile}>
+          <Header>Minimalist</Header>
+          <Text>john-tierney.com</Text>
+          <Divider fullWidth />
+          <PhotoInfo
+            photo={{
+              uri:
+                'https://scontent-lht6-1.cdninstagram.com/vp/8b749f16d5cadd6e680b479af5f237b0/5B68909F/t51.2885-19/10963846_377118132473154_1350059129_a.jpg'
+            }}
+            title="john_tierney_art"
+          />
+          <Photo
+            imageURL={
+              'https://scontent-lht6-1.cdninstagram.com/vp/43d6f9d19b29dbfc7b58c46a92ed425c/5B5770F5/t51.2885-15/e35/30602084_186676038631423_7930424840232632320_n.jpg'
+            }
+          />
+        </View>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# React Native Minimalist Expo
+# React Native Minimalist in Expo
+
+This is a sample repository to demonstrate using [React Native Minimalist](https://github.com/applification/react-native-minimalist) in Expo.
+
+## Installation
+
+### Install dependencies
+
+```bash
+npm install
+```
+
+## Running the App
+
+```bash
+exp start
+```
+
+Then follow the instructions in the console to run the app using Expo.
